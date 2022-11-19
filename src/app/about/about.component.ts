d138eabbaee13690bb9a7de51b492736fea2a128
draco-se <73908519+draco-se@users.noreply.github.com>
2022-11-19 16:48:00 +0100

    completed about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  intent: string = encodeURIComponent('Hello @__sinofgluttony');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
