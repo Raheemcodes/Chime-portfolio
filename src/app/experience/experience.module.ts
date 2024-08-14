cb966b8da9bc109aecd94e2ae9d91e889f9eea9e
Raheemcodes <raheemscorporatemail@gmail.com>
2024-08-14 11:16:10 +0100

    Add footer to experience page

--- src/app/experience/experience.module.ts
+++ src/app/experience/experience.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CheffingComponent } from '../components/cheffing/cheffing.component';
 import { ExperienceComponent } from './experience.component';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
   declarations: [ExperienceComponent, CheffingComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: ExperienceComponent }]),
+    FooterModule,
   ],
 })
 export class ExperienceModule {}
